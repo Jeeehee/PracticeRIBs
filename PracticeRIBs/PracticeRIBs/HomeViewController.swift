ad9c1a5a0df6d8f4be2d8616dbbea199fe4ad09c
jiheeHwang <jhhwang@autocrypt.io>
2023-12-15 10:08:26 +0900

    chore: 초기세팅

---
+++ PracticeRIBs/PracticeRIBs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  PracticeRIBs
+//
+//  Created by Jihee hwang on 12/15/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+    }
+
+}
+
